81ecc20fd7735757d32b56f8481bebe88efd8926
Eric Vandenberg <Eric1827v@gmail.com>
2015-02-19 15:09:21 -0800

    More File pushing

---
+++ Tic-Tac-Toe-game/js/main.js
@@ -0,0 +1,21 @@
+	//When the first player clicks the first box(i.e. no other boxes are clicked) alert a prompt to ask if player wants to select 'X' or 'O'
+
+//populate the assign_xo div one and two
+//Automatically populate player 2 playerID as 'O' and assign_xo two
+
+	//Create something that assigns their choice to their turn
+			//Add classlist and add document.querySelect innerHTML 'X'
+			
+		//create num_index that alternates between 0 and 1
+		//use this num_index to specify who's turn it is
+
+//Create a onclick function that applies player id to the box clicked
+
+//Store the players selection into their respective array
+
+//Each player has their own array
+//Make sure the array is always sorted
+
+//Create win possibility array
+
+//Compare the individuals array with the winArray
